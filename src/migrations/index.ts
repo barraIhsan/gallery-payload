3fe4fca224e8a7514940d6cea40313a7f6544b61
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2025-08-21 15:17:48 +0700

    fix: update migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,4 +1,5 @@
-import * as migration_20250815_145302_initial from './20250815_145302_initial'
+import * as migration_20250815_145302_initial from './20250815_145302_initial';
+import * as migration_20250821_081731 from './20250821_081731';
 
 export const migrations = [
   {
@@ -6,4 +7,9 @@ export const migrations = [
     down: migration_20250815_145302_initial.down,
     name: '20250815_145302_initial',
   },
-]
+  {
+    up: migration_20250821_081731.up,
+    down: migration_20250821_081731.down,
+    name: '20250821_081731'
+  },
+];
